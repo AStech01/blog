acdb2c2a0b26a52f2ccabb58137ac90d1a5719f4
AStech01 <ahamedsabith12345@gmail.com>
2025-11-14 11:53:51 +0530

    yes

--- mern-blog-frontend/src/api/api.js
+++ mern-blog-frontend/src/api/api.js
@@ -1,59 +1,14 @@
-
-
-
-// import axios from "axios";
-
-// const API = axios.create({
-//   baseURL: "http://localhost:5000/api", // make sure this matches your backend
-// });
-
-// // Add token to every request if available
-// API.interceptors.request.use((config) => {
-//   const token = localStorage.getItem("token");
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// }, (error) => {
-//   return Promise.reject(error);
-// });
-
-// export default API;
-
-
-// import axios from "axios";
-
-// const API = axios.create({
-//   baseURL: "http://localhost:5000/api",
-// });
-
-// API.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem("token");
-//     if (token) config.headers.Authorization = `Bearer ${token}`;
-//     return config;
-//   },
-//   (error) => Promise.reject(error)
-// );
-
-// export default API;
-
-
 import axios from "axios";
 
-// Use environment variable for backend URL
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL, // <-- use VITE_API_URL from .env
+  // Prefer env var in production; fallback to local server + /api prefix
+  baseURL:
+    (typeof process !== "undefined" &&
+      process.env.REACT_APP_API_BASE &&
+      process.env.REACT_APP_API_BASE.replace(/\/$/, "")) ||
+    "http://localhost:5000/api",
+  // include credentials if you rely on cookies (optional)
+  // withCredentials: true,
 });
-
-// Attach token to headers automatically
-API.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) config.headers.Authorization = `Bearer ${token}`;
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
 
 export default API;
