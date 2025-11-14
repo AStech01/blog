a77330f5d52f3833b586ef724af87113b80838df
AStech01 <ahamedsabith12345@gmail.com>
2025-11-14 09:47:35 +0530

    message

--- mern-blog-frontend/src/api/api.js
+++ mern-blog-frontend/src/api/api.js
@@ -21,12 +21,32 @@
 // export default API;
 
 
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "http://localhost:5000/api",
+// });
+
+// API.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem("token");
+//     if (token) config.headers.Authorization = `Bearer ${token}`;
+//     return config;
+//   },
+//   (error) => Promise.reject(error)
+// );
+
+// export default API;
+
+
 import axios from "axios";
 
+// Use environment variable for backend URL
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL, // <-- use VITE_API_URL from .env
 });
 
+// Attach token to headers automatically
 API.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
